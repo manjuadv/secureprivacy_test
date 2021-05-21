a2a729643213d222a5068d1b794011f271f9d77e
manjuadv <pathmakumaraad@gmail.com>
2021-05-21 15:47:17 +0530

    task 2 logic done

--- secure_privacy_test/Program.cs
+++ secure_privacy_test/Program.cs
@@ -6,7 +6,55 @@ namespace secure_privacy_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("project setup succeeded");
+            CheckString("100");
+            CheckString("101");
+            CheckString("1");
+            CheckString("10");
+            CheckString("1100");
+            CheckString("11010");
+            CheckString("110010");
+        }
+        private static void CheckString(string binaryString)
+        {
+            Console.WriteLine(binaryString + " : " + IsGoodBinaryString(binaryString).ToYesNo());
+        }
+        private static bool IsGoodBinaryString(string binaryString)
+        {
+            char[] digits = binaryString.ToCharArray();
+            int count_ones = 0;
+            foreach (var digit in digits)
+            {
+                if (digit == '1')
+                {
+                    count_ones++;
+                }
+                else if (digit == '0')
+                {
+                    count_ones--;
+                }
+                else
+                {
+                    return false;
+                }
+                if (count_ones < 0)
+                {
+                    return false;
+                }
+            }
+            if (count_ones == 0)
+                return true;
+            return false;
+        }
+    }
+
+    public static class Extentions
+    {
+        public static string ToYesNo(this bool result)
+        {
+            string yesOrNo = "NO";
+            yesOrNo = result ? "YES" : "NO";
+
+            return yesOrNo;
         }
     }
 }
