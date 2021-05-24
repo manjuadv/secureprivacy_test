972aa3b15abd128778ae9d2362f71915e4d1b77c
manjuadv <pathmakumaraad@gmail.com>
2021-05-24 12:28:11 +0530

    Order api refactore-2

--- DataAccess/Repositories/OrderRepository.cs
+++ DataAccess/Repositories/OrderRepository.cs
@@ -15,37 +15,4 @@ namespace Task1.DataAccess.Repositories
 
         }
     }
-    /*public class OrderRepository : IOrderRepository
-    {
-        private readonly OrderService _orderService;
-
-        public OrderRepository(OrderService orderService)
-        {
-            _orderService = orderService;
-        }
-        public async Task Create(Order order)
-        {
-            await _orderService.Insert(order);
-        }
-
-        public async Task Delete(string id)
-        {
-            await _orderService.Remove(id);
-        }
-
-        public async Task<IEnumerable<Order>> Get()
-        {
-            return await _orderService.Get();
-        }
-
-        public async Task<Order> Get(string id)
-        {
-            return await _orderService.Get(id);
-        }
-
-        public async Task Update(string id, Order order)
-        {
-            await _orderService.Update(id, order);
-        }
-    }*/
 }
